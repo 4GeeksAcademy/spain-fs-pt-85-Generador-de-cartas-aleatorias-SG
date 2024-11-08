752a6a23cea92459f9d7e5e795198b7ea4044cdd
Sergi-03 <robrovskyyt@gmail.com>
2024-11-08 19:46:36 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbolArray = ["♦", "♥", "♠", "♣"];
+  let cardNumberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+  let randomSymbolArray =
+    symbolArray[Math.floor(Math.random() * symbolArray.length)];
+  let randomCardNumber =
+    cardNumberArray[Math.floor(Math.random() * cardNumberArray.length)];
+
+  document.getElementById("upperSymbol").textContent = randomSymbolArray;
+  document.getElementById("cardNumber").textContent = randomCardNumber;
+  document.getElementById("lowerSymbol").textContent = randomSymbolArray;
+
+  if (randomSymbolArray === "♥" || randomSymbolArray === "♦") {
+    document.getElementById("upperSymbol").style.color = "red";
+    document.getElementById("lowerSymbol").style.color = "red";
+  }
 };
